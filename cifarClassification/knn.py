60ab85d2be0deae0a99506695788d723a3f9b39f
jie.dai <daijie@kuaikuaidai.com>
2017-08-24 18:02:34 +0800

    fdf

--- cifarClassification/knn.py
+++ cifarClassification/knn.py
@@ -1,1 +1,9 @@
 import util
+
+cifar10_path="/cifar-10-batches-py"
+
+x_train,y_train=load_cifar10_batch(cifar10_path,1)
+for i in range(2,6):
+    features,labels=load_cifar10_batch(cifar10_path,i)
+    x_train,y_train=np.concetenate([x_train,features]),np.concatenate([y_train,labels])
+    
--- cifarClassification/util.py
+++ cifarClassification/util.py
@@ -1,2 +1,16 @@
+import cPickle as pickle 
+
 def load_cifar10_batch(cifar10_dataset_folder_path,batch_id):
-    with open(cifar10_dataset_folder_path=)
+    with open(cifar10_dataset_folder_path='/data_batch_'+str(batch_id),mode='rb') as file:
+        batch=pickle.load(file,encoding='latinl')
+    features=batch['data'].reshape((len(batch['data']),3,32,32)).transpose(0,2,3,1)
+    labels=batch['labels']
+    return features,labels
+
+
+def load_cifar10_test(cifar10_dataset_folder_path):
+    with open(cifar10_dataset_folder_path='/test_batch',mode='rb') as file:
+        batch=pickle.load(file,encoding='latinl')
+    features=batch['data'].reshape((len(batch['data']),3,32,32)).transpose(0,2,3,1)
+    labels=batch['labels']
+    return features,labels
